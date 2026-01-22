b3ed92ac3d643ee03ca6108fa3ea1dcd7a2bbcbc
Zubillaga736 <anonimoxd811@gmail.com>
2026-01-22 11:48:36 +0100

    commit del main (menu)

--- app/src/main/java/com/saseiv/MainActivity.java
+++ app/src/main/java/com/saseiv/MainActivity.java
@@ -1,6 +1,10 @@
 package com.saseiv;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void openProfile(MenuItem menu){
+        Intent intent = new Intent(MainActivity.this, ProfileScreen.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
